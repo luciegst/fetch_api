33a1ce45bb060105acc47cd030e11df47b006160
Lucie Genest <luciegenest@hotmail.fr>
2019-01-30 17:22:29 +0100

    ajout et suppression d'un element

--- src/Petlist.jsx
+++ src/Petlist.jsx
@@ -8,10 +8,13 @@ class Petlist extends Component {
     super(props);
     this.state = {
       lists: [],
+      id: 500000,
       pseudo: '',
       breed: '',
     }
     this.changeInput = this.changeInput.bind(this);
+     this.addPet = this.addPet.bind(this);
+     this.deletePet = this.deletePet.bind(this);
   }
 
   changeInput(e) {
@@ -30,64 +33,92 @@ class Petlist extends Component {
       });
   }
 
-  render() {
-    const listsfilter = this.state.lists.slice(1,11)
-    return (
-      <div>
-        <Container className="form-container">
-          <Row>
-            <Col lg="4">
-              <Form>
-                <FormGroup>
-                  <Input
-                    type="text"
-                    name="pseudo"
-                    id="examplepseudo"
-                    value={this.state.pseudo}
-                    onChange={this.changeInput}
-                    placeholder="Pseudo"
-                    required
-                  />
-                </FormGroup>
-              </Form>
-            </Col>
-            <Col lg="4">
+  deletePet(id){
+    const newlist = this.state.lists.filter(item=> item.resource.id !== id)
+    this.setState({
+      lists: newlist
+    })
+  }
+
+  addPet() {
+    this.setState({
+      id: this.state.id + 1
+    })
+    const obj = {
+      resource: {
+        avatar: {
+          thumb: "../../assets/petpaw.jpeg"
+        },
+        breed: {
+          lib: this.state.breed
+        },
+        id: this.state.id,
+        pseudo: this.state.pseudo
+      }
+    };
+    this.setState({
+      lists: [...this.state.lists, obj]
+    })
+  };
+
+render() {
+  const listsfilter = this.state.lists.slice(-10)
+  return (
+    <div>
+      <Container className="form-container">
+        <Row>
+          <Col lg="4">
+            <Form>
               <FormGroup>
                 <Input
                   type="text"
-                  name="breed"
-                  id="examplebreed"
-                  value={this.state.breed}
+                  name="pseudo"
+                  id="examplepseudo"
+                  value={this.state.pseudo}
                   onChange={this.changeInput}
-                  placeholder="Breed"
+                  placeholder="Pseudo"
                   required
                 />
               </FormGroup>
-            </Col>
-            <Col lg="2">
-              <Button>ADD PET</Button>
-            </Col>
-          </Row>
-        </Container>
-        <Container>
-          <Row>
-            <Col lg="12">
-                <ol>
-                {listsfilter.map(list => (
-                  <li>
-                    <img src={list.resource.avatar.thumb} alt="thumb" width="30px" />
-                    {list.resource.pseudo}
-                    {list.resource.breed.lib}
-                    <button>Delete</button>
-                  </li>
+            </Form>
+          </Col>
+          <Col lg="4">
+            <FormGroup>
+              <Input
+                type="text"
+                name="breed"
+                id="examplebreed"
+                value={this.state.breed}
+                onChange={this.changeInput}
+                placeholder="Breed"
+                required
+              />
+            </FormGroup>
+          </Col>
+          <Col lg="2">
+            <Button onClick={this.addPet}>ADD PET</Button>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col lg="12">
+            <ol>
+              {listsfilter.map(list => (
+                <li key={list.resource.id}>
+                  <img src={list.resource.avatar.thumb} alt="thumb" width="30px" />
+                  {list.resource.pseudo}
+                  {list.resource.breed.lib}
+                  <button onClick={() => this.deletePet(list.resource.id)}>Delete</button>
+                </li>
               ))}
-               </ol>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+            </ol>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
 }
 
 export default Petlist;
